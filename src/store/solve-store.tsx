1f4a688eab29377a78a8776d24bcf03d8b4fccaf
Laura buns <hi@laura.monster>
2024-06-27 09:02:22 +0100

    star

--- src/store/solve-store.tsx
+++ src/store/solve-store.tsx
@@ -145,6 +145,7 @@ const makeSolveStore = (
 
 // http://localhost:1234/solve/3-1,1-7,1,2-9,1,2-9,5-9,1,2-7,1,2-1,1-3/3-5-5-5-5-1,5,1-1,5,1-1,5,1-1,3,1-1,1-3-1-1-5-7
 // http://localhost:1234/solve/1,2-1,1-0-1,1-1,2/1,1-0-1,1-1,1-2,2
+// http://localhost:1234/solve/5,9-5,7-5,4-5,1-5,1-3,1-1,3,2-3-1,3,2-3,1-5,1-5,1-5,4-5,7-5,9/7,7-6,6-6,6-5,5-5,5-1,1-1,1,1,1-2,1,1,2-3,3-4,4-3,3-3,3-2,1,2-2,3,2-1,7,1
 
 const SolveStoreCtx =
   createContext<React.MutableRefObject<ReturnType<typeof makeSolveStore>>>(
